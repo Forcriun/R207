b4532e0cf0586ca66d75875574df89dfb701a467
Forcriun <forcriun@gmail.com>
2018-02-02 22:57:16 +0100

    Prueba de test con Gradle y Travis

---
+++ src/main/java/EjercicioR207.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+class EjercicioR207
+{    
+    public String devolverEnOrden(ArrayList<String> palabras){
+        ArrayList<String> coleccion = new ArrayList(palabras);
+        String cadenaADevolver = "";
+
+        if (coleccion.size() > 0){
+            while(coleccion.size() > 0){
+                String palabraMasLarga = coleccion.get(0);
+                int x = 0;  // Contador del bucle
+                int posicionMasLarga = 0;
+
+                while(x < coleccion.size()){
+                    if(coleccion.get(x).length() >= palabraMasLarga.length()){
+                        palabraMasLarga = coleccion.get(x);
+                        posicionMasLarga = x;
+                    }
+                    x++;
+                }
+
+                cadenaADevolver += palabraMasLarga + ",";
+                coleccion.remove(posicionMasLarga);
+            }
+            cadenaADevolver = cadenaADevolver.substring(0,cadenaADevolver.length()-1);
+        }
+
+        return cadenaADevolver;
+    }
+
+    // A continuación puedes escribir otros métodos si lo necesitas...
+}
